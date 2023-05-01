606360773bdaebb1f517c8a6d15ad12ef3882194
amontalvof <amontalvof92@gmail.com>
2023-05-01 00:47:01 -0400

    feat: Create Component from CLI

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-books',
+    templateUrl: './books.component.html',
+    styleUrls: ['./books.component.css'],
+})
+export class BooksComponent {
+    disableButton = false;
+    books = [
+        {
+            name: 'Clean Code',
+            author: 'Robert C. Martin',
+            imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX258_BO1,204,203,200_.jpg',
+        },
+        {
+            name: 'The Pragmatic Programmer',
+            author: 'Andrew Hunt and David Thomas',
+            imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg',
+        },
+    ];
+
+    handleClick() {
+        this.disableButton = true;
+    }
+}
