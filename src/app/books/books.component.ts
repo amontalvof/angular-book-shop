8aced0ca1f6911366cc9d1de9e804d39ef71a3dd
amontalvof <amontalvof92@gmail.com>
2023-05-06 12:42:27 -0400

    feat: style component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -19,6 +19,18 @@ export class BooksComponent {
             imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg',
             price: 30,
         },
+        {
+            name: 'art of computer programming',
+            author: 'donald jhon fuller',
+            imgSrc: 'https://m.media-amazon.com/images/I/41msKv2oN9L._AC_UF894,1000_QL80_.jpg',
+            price: 50,
+        },
+        {
+            name: 'introduction to algorithms',
+            author: 'thomas h. cormen',
+            imgSrc: 'https://m.media-amazon.com/images/I/61Pgdn8Ys-L._AC_UF1000,1000_QL80_.jpg',
+            price: 40,
+        },
     ];
     isShowing = true;
 }
