8a4df9a18dde46a4dcfd7ba1b6850b1f7dde6a3c
amontalvof <amontalvof92@gmail.com>
2023-05-17 10:01:47 -0400

    feat: login register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { BooksComponent } from './components/books/books.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 const routes: Routes = [
     { path: '', component: BooksComponent },
     { path: 'cart', component: CartComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
