1073bdc8aa0768e8e6041b6eebf8667584201c48
amontalvof <amontalvof92@gmail.com>
2023-05-18 01:30:33 -0400

    feat: angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { BooksModule } from './components/books/books.module';
 import { CartComponent } from './components/cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
     declarations: [AppComponent, CartComponent],
-    imports: [BrowserModule, FormsModule, AppRoutingModule, BooksModule],
+    imports: [BrowserModule, AuthModule, AppRoutingModule, BooksModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { RegisterForm } from 'src/app/types/Auth';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+    form: RegisterForm = {
+        email: '',
+        password: '',
+        confirm_password: '',
+    };
 
+    onSubmit(): void {
+        alert(JSON.stringify(this.form));
+    }
 }
